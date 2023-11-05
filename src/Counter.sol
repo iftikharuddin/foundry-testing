54bb00bd5980a6356d247a22070ce9e87e477168
Iftikhar uddin <iftipro@gmail.com>
2023-11-05 23:41:27 +0500

    logging in testings

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+import {console} from "forge-std/console.sol";
+
 contract Counter {
-    uint public count;
+    uint256 public count;
 
     // Function to get the current count
-    function get() public view returns (uint) {
+    function get() public view returns (uint256) {
         return count;
     }
 
     // Function to increment count by 1
     function inc() public {
+
         count += 1;
     }
 
@@ -18,5 +21,15 @@ contract Counter {
     function dec() public {
         // This function will fail if count = 0
         count -= 1;
+    }
+
+    // Function to increment count by 1
+    function testConsole() public {
+        console.log("Before increment", count);
+        count += 1;
+        console.log("After increment", count);
+        int newVar = -1;
+        console.logInt(newVar);
+
     }
 }
--- src/Imports.sol
+++ src/Imports.sol
@@ -3,6 +3,4 @@ pragma solidity 0.8.20;
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
-contract Token is ERC20("Name", "N", 18) {
-
-}
+contract Token is ERC20("Name", "N", 18) {}
