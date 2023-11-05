e4a5836e3ebb3c5310c096e8d7d077cc543e6b8a
Iftikhar uddin <iftipro@gmail.com>
2023-11-05 23:59:19 +0500

    All about prank in testings

--- test/Auth.t.sol
+++ test/Auth.t.sol
@@ -13,5 +13,25 @@ contract AuthTest is Test {
         wallet = new Wallet();
     }
 
+    function testSetOwner() public {
+        wallet.setOwner(address (1));
+        assertEq(wallet.owner(), address (1));
+    }
+
+    function testFailNotOwner() public {
+        vm.prank(address (1)); // this means for the next trx make owner address(1) and he should be calling the func
+        wallet.setOwner(address (1));
+    }
+
+    function testFailSetOwnerAgain() public {
+        // startPrank set the owner for multiple calls/trx
+        vm.startPrank(address (1));
+        // msg.sender = address (1)
+        wallet.setOwner(address (1));
+
+        // for stopping the behaviour
+        vm.stopPrank();
+
+    }
 
 }
