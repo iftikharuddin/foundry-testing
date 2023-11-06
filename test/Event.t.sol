1c8d087934e8bbdaccaeeef6541d7533515fdcf6
Iftikhar uddin <iftipro@gmail.com>
2023-11-06 11:23:27 +0500

    Events testings foundry

--- test/Event.t.sol
+++ test/Event.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity 0.8.20;
 
 import "forge-std/Test.sol";
 import {Event} from "../src/Event.sol";
@@ -13,6 +13,50 @@ contract EventTest is Test {
 
     function setUp() public {
         e = new Event();
+    }
+
+    function testEmitTransferEvent() public {
+        // function expectEmit(
+        //     bool checkTopic1,
+        //     bool checkTopic2,
+        //     bool checkTopic3,
+        //     bool checkData
+        // ) external;
+
+        // 1. Tell Foundry which data to check
+        // Check index 1, index 2 and data
+        vm.expectEmit(true, true, false, true);
+        // 2. Emit the expected event
+        emit Transfer(address(this), address(123), 456);
+        // 3. Call the function that should emit the event
+        e.transfer(address(this), address(123), 456);
+
+        // Check only index 1
+        vm.expectEmit(true, false, false, false);
+        emit Transfer(address(this), address(123), 456);
+        // NOTE: index 2 and data (amount) doesn't match
+        //       but the test will still pass
+        e.transfer(address(this), address(111), 222);
+    }
+
+    function testEmitManyTransferEvent() public {
+        address[] memory to = new address[](2);
+        to[0] = address(111);
+        to[1] = address(222);
+
+        uint256[] memory amounts = new uint[](2);
+        amounts[0] = 1;
+        amounts[1] = 2;
+
+        for (uint256 i = 0; i < to.length; i++) {
+            // 1. Tell Foundry which data to check
+            vm.expectEmit(true, true, false, true);
+            // 2. Emit the expected event
+            emit Transfer(address(this), to[i], amounts[i]);
+        }
+
+        // 3. Call the function that should emit the event
+        e.transferMany(address(this), to, amounts);
     }
 
 }
