49e48b2a5a579b850d425511da8a7e34ae19c536
Iftikhar uddin <iftipro@gmail.com>
2023-11-06 11:05:00 +0500

    Custom errors tstings, labeling them and many more

--- test/Error.t.sol
+++ test/Error.t.sol
@@ -11,12 +11,34 @@ contract ErrorTest is Test {
         err = new Error();
     }
 
+    // expected to throw an error
     function testFail() public {
         err.throwError();
     }
 
+    // vm.expectRevert
     function testRevert() public {
         vm.expectRevert();
         err.throwError();
+    }
+
+    // vm.expectRevert with param
+    function testRequireMessage() public {
+        vm.expectRevert(bytes ("not authorized"));
+        err.throwError();
+    }
+
+
+    // vm.expectRevert with custom error param
+    function testCustomError() public {
+        vm.expectRevert(Error.NotAuthorized.selector);
+        err.throwCustomError();
+    }
+
+    function testErrorLabel() public {
+        assertEq(uint256(1), uint256(1), "Label 1");
+        assertEq(uint256(1), uint256(1), "Label 2");
+        assertEq(uint256(1), uint256(1), "Label 3");
+        assertEq(uint256(1), uint256(1), "Label 4");
     }
 }
