d71602eaf96bcfb7606a6eac909e710d75196e4b
Iftikhar uddin <iftipro@gmail.com>
2023-11-13 11:03:12 +0500

    Fuzz tests testing with Foundry

---
+++ test/Fuzz.t.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+import {Bit} from "../src/Bit.sol";
+
+// forge test --match-path test/Fuzz.t.sol
+
+// Topics
+// - fuzz
+// - assume and bound
+// - stats
+//   (runs: 256, μ: 18301, ~: 10819)
+//   runs - number of tests
+//   μ - mean gas used
+//   ~ - median gas used
+
+contract FuzzTest is Test {
+    Bit public b;
+
+    function setUp() public {
+        b = new Bit();
+    }
+
+    function mostSignificantBit(uint256 x) private pure returns (uint256) {
+        uint256 i = 0;
+        while ((x >>= 1) > 0) {
+            i++;
+        }
+        return i;
+    }
+
+    function testMostSignificantBitManual() public {
+        assertEq(b.mostSignificantBit(0), 0);
+        assertEq(b.mostSignificantBit(1), 0);
+        assertEq(b.mostSignificantBit(2), 1);
+        assertEq(b.mostSignificantBit(4), 2);
+        assertEq(b.mostSignificantBit(8), 3);
+        assertEq(b.mostSignificantBit(type(uint256).max), 255);
+    }
+
+    function testMostSignificantBitFuzz(uint256 x) public {
+        // assume - If false, the fuzzer will discard the current fuzz inputs
+        //          and start a new fuzz run
+        // Skip x = 0
+        // vm.assume(x > 0);
+        // assertGt(x, 0);
+
+        // bound(input, min, max) - bound input between min and max
+        // Bound
+        x = bound(x, 1, 10);
+        // assertGe(x, 1);
+        // assertLe(x, 10);
+
+        uint256 i = b.mostSignificantBit(x);
+        assertEq(i, mostSignificantBit(x));
+    }
+}
