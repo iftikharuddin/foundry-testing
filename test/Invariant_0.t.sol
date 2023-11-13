79594ab9591a4cd815e30062edca52f6efade98b
Iftikhar uddin <iftipro@gmail.com>
2023-11-13 11:12:19 +0500

    Invariants in foundry, mastering the art of testing

---
+++ test/Invariant_0.t.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+// Topics
+// - Invariant
+// - Difference between fuzz and invariant
+// - Failing invariant
+// - Passing invariant
+// - Stats - runs, calls, reverts
+
+contract InvariantIntro {
+    bool public flag;
+
+    function func_1() external {}
+    function func_2() external {}
+    function func_3() external {}
+    function func_4() external {}
+
+    function func_5() external {
+        flag = true;
+    }
+}
+
+contract IntroInvariantTest is Test {
+    InvariantIntro private target;
+
+    function setUp() public {
+        target = new InvariantIntro();
+    }
+
+    function invariant_flag_is_always_false() public {
+        assertEq(target.flag(), false);
+    }
+}
