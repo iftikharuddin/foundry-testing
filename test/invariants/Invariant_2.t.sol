fac9804a8c923ad2bfd7bb24b97ca02e85b27c42
Iftikhar uddin <iftipro@gmail.com>
2023-11-14 12:27:36 +0500

    Invariants with less revert testings

---
+++ test/invariants/Invariant_2.t.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import {WETH} from "../../src/WETH.sol";
+
+// Topics
+// - handler based testing - test functions under specific conditions
+// - target contract
+// - target selector
+
+import {CommonBase} from "forge-std/Base.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import {StdUtils} from "forge-std/StdUtils.sol";
+
+contract Handler is CommonBase, StdCheats, StdUtils {
+    WETH private weth;
+    uint256 public wethBalance;
+    uint256 public numCalls;
+
+    constructor(WETH _weth) {
+        weth = _weth;
+    }
+
+    receive() external payable {}
+
+    function sendToFallback(uint256 amount) public {
+        amount = bound(amount, 0, address(this).balance);
+        wethBalance += amount;
+        numCalls += 1;
+
+        (bool ok,) = address(weth).call{value: amount}("");
+        require(ok, "sendToFallback failed");
+    }
+
+    function deposit(uint256 amount) public {
+        amount = bound(amount, 0, address(this).balance);
+        wethBalance += amount;
+        numCalls += 1;
+
+        weth.deposit{value: amount}();
+    }
+
+    function withdraw(uint256 amount) public {
+        amount = bound(amount, 0, weth.balanceOf(address(this)));
+        wethBalance -= amount;
+        numCalls += 1;
+
+        weth.withdraw(amount);
+    }
+
+    function fail() external {
+        revert("fail");
+    }
+}
+
+contract WETH_Handler_Based_Invariant_Tests is Test {
+    WETH public weth;
+    Handler public handler;
+
+    function setUp() public {
+        weth = new WETH();
+        handler = new Handler(weth);
+
+        // Send 100 ETH to handler
+        deal(address(handler), 100 * 1e18);
+        // Set fuzzer to only call the handler
+        targetContract(address(handler));
+
+        bytes4[] memory selectors = new bytes4[](3);
+        selectors[0] = Handler.deposit.selector;
+        selectors[1] = Handler.withdraw.selector;
+        selectors[2] = Handler.sendToFallback.selector;
+
+        // Handler.fail() not called
+        targetSelector(FuzzSelector({addr: address(handler), selectors: selectors}));
+    }
+
+    function invariant_eth_balance() public {
+        assertGe(address(weth).balance, handler.wethBalance());
+        console.log("handler num calls", handler.numCalls());
+    }
+}
